c697a43cc23ed769b59a2d8b5adef5d2bf08a03b
smoming <smoming1031@gmail.com>
2017-04-08 00:47:59 +0800

    RelativeLayout_Example
    RelativeLayout Example

---
+++ Layout_Relative/Layout_RelativePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Layout_Relative
+{
+	public partial class Layout_RelativePage : ContentPage
+	{
+		public Layout_RelativePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
